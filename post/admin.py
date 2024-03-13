67b1c608545917bdfbcadf219867f5eaa1650644
엄영철 <52156476+Um-king@users.noreply.github.com>
2024-03-14 01:13:13 +0900

    프로젝트 파일 수정

--- post/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post, Tag, Itinerary, Mission, Comment
-
-
-admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Itinerary)
-admin.site.register(Mission)
-admin.site.register(Comment)
